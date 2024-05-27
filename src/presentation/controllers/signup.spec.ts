11bdcd87a0ec5b50f66164b17309ac6ae5848959
ViniciusAcordiSoethe <vinicius.soethe@proton.me>
2024-05-26 21:17:32 -0300

    refactor: customizando erros

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -27,6 +27,6 @@ describe('SignUp Controller', () => {
         }
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
-        expect(httpResponse.body).toEqual(new MissingParamsError('email'))
+        expect(httpResponse.body).toEqual(new MissingParamsError('emails'))
     })
 })
