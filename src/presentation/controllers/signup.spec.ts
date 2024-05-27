43a75ee129bc449de52a83ec7722028163aa149f
ViniciusAcordiSoethe <vinicius.soethe@proton.me>
2024-05-26 21:37:15 -0300

    fix: ajustando Husky para a nova verção

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -27,6 +27,6 @@ describe('SignUp Controller', () => {
         }
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
-        expect(httpResponse.body).toEqual(new MissingParamsError('emails'))
+        expect(httpResponse.body).toEqual(new MissingParamsError('email'))
     })
 })
