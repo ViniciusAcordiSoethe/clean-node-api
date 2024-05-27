63fb2e291f19d273a868b27162ee4fbcb36ec523
ViniciusAcordiSoethe <vinicius.soethe@proton.me>
2024-05-26 21:54:43 -0300

    refactor: criando o protocolo para a controller

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import { HttpRequest , HttpResponse} from "../protocols/http"
 import { MissingParamsError } from "../errors/missing-params-error"
-import {badRequest} from "../helpers/http-helper"
+import { badRequest } from "../helpers/http-helper"
+import { Controller } from "../protocols/controller"
  
-export class SignUpController {
+export class SignUpController implements Controller {
     handle(httpRequest: HttpRequest) : HttpResponse {
         const requiredFields = ['name', 'email', 'password']
 
